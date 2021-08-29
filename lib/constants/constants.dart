90ca2db8efe457749b758cb6e330fb0a9c165e69
dchantez <dchtipro@gmail.com>
2021-08-29 01:17:11 -0400

    constants.dart

---
+++ lib/constants/constants.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+const kGreenInfo = Color(0xFF1FBF2F);
+const kRedInfo = Color(0xFFBF461F);
+const kGreyInfo = Color(0xFFC4C4C4);
+const kInactiveText = Color(0xFF464646);
+const kActiveText = Color(0xFF3186E7);
+
+const kLinearGradientBlue =
+    LinearGradient(colors: [Color(0xFF2A8DE8), Color(0xFF1962A6)]);
+
+const kLinearGradientBlack =
+    LinearGradient(colors: [Color(0xFF606060), Color(0xFF2C2B2B)]);
+
+const kTitleScaffold = TextStyle(
+  fontSize: 18,
+  color: Color(0xFF3186E7),
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+);
+
+const kHeaderRegister = TextStyle(
+  fontSize: 34,
+  color: Color(0xFF464646),
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+);
+
+const kHeaderBodyPassword = TextStyle(
+  fontSize: 20,
+  color: Color(0xFF464646),
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+);
+
+const kHeaderBody = TextStyle(
+  fontSize: 14,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+);
+
+const kBalanceMainScreen = TextStyle(
+  fontSize: 48,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+  color: Color(0xFFFFFFFF),
+);
+
+const kFooterTextMainScreen = TextStyle(
+  fontSize: 16,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w700,
+  color: Color(0xFFFFFFFF),
+);
