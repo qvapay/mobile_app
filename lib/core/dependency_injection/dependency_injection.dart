71cfb6c2de6cb0e6138b87b5e1767b4507e7111d
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2021-10-29 21:56:58 -0500

    fix(user_data): include `UserDataRepository` in dependency injection
    - created instance of `HydratedBloc`

--- lib/core/dependency_injection/dependency_injection.dart
+++ lib/core/dependency_injection/dependency_injection.dart
@@ -2,10 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mobile_app/authentication/authentication.dart';
 import 'package:mobile_app/core/dependency_injection/oauth_secure_storage.dart';
 import 'package:mobile_app/features/home/home.dart';
+import 'package:mobile_app/features/user_data/user_data.dart';
 import 'package:mobile_app/preferences/preferences.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:qvapay_api_client/qvapay_api_client.dart';
 
 final getIt = GetIt.instance;
@@ -14,6 +17,10 @@ Future<void> setUp() async {
   /// Local Storage
   await Hive.initFlutter();
   final preferencesBox = await Hive.openBox<Map>('preferences');
+
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
 
   getIt
 
@@ -31,5 +38,11 @@ Future<void> setUp() async {
     )
     ..registerLazySingleton<IHomeRepository>(
       () => HomeRepository(qvaPayApi: getIt<QvaPayApi>()),
+    )
+    ..registerLazySingleton<IUserDataRepository>(
+      () => UserDataRepository(
+        qvaPayApi: getIt<QvaPayApi>(),
+        preferencesRepository: getIt<PreferencesRepository>(),
+      ),
     );
 }
