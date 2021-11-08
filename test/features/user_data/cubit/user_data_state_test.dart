0dae75db1ca824c2d424c298750efdb4321dea27
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2021-11-08 13:42:43 -0600

    test(user_data_cubit): reach 100% test coverage

--- test/features/user_data/cubit/user_data_state_test.dart
+++ test/features/user_data/cubit/user_data_state_test.dart
@@ -1,5 +1,26 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile_app/features/user_data/user_data.dart';
+
+class FakeUserData extends Fake implements UserData {}
 
 void main() {
-  test('description', () {});
+  group('UserDataState', () {
+    test('supports value comparisons', () {
+      expect(const UserDataState(), const UserDataState());
+    });
+    final tFakeUserData = FakeUserData();
+    test('supports value comparisons', () {
+      expect(
+        UserDataState(userData: tFakeUserData),
+        UserDataState(userData: tFakeUserData),
+      );
+    });
+
+    test('copyWith', () {
+      expect(
+        const UserDataState().copyWith(),
+        const UserDataState().copyWith(),
+      );
+    });
+  });
 }
