aa08d9fe88419a9b9bac702ddaffbdbe50a1cf78
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2021-11-18 22:17:35 -0600

    feat(transactions_repository): add `processTransaction` method

--- lib/features/transactions/repository/transactions_repository.dart
+++ lib/features/transactions/repository/transactions_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:mobile_app/core/error/failures.dart';
+import 'package:mobile_app/core/extensions/extensions.dart';
 import 'package:mobile_app/features/user_data/models/models.dart';
 import 'package:qvapay_api_client/qvapay_api_client.dart';
 
@@ -8,6 +9,11 @@ abstract class ITransactionsRepository {
     String? searchTerm,
     DateTime? start,
     DateTime? end,
+  });
+
+  Future<Either<Failure, UserTransaction>> processTransaction({
+    required UserTransaction transaction,
+    String pin = '0000',
   });
 }
 
@@ -35,6 +41,35 @@ class TransactionsRepository extends ITransactionsRepository {
 
       return Right(userTransactions);
     } catch (e) {
+      return const Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserTransaction>> processTransaction({
+    required UserTransaction transaction,
+    String pin = '0000',
+  }) async {
+    try {
+      final transactionToPay = await _qvaPayApi.createTransaction(
+        uuid: transaction.uuid,
+        amount: transaction.amount.toDouble(),
+        description: transaction.description,
+      );
+
+      final transactionPaid = await _qvaPayApi.payTransaction(
+        uuid: transactionToPay.uuid,
+        pin: pin,
+      );
+
+      return Right(UserTransaction.fromTransaction(transactionPaid));
+    } catch (e) {
+      if (e is TransactionException) {
+        return Left(UserTransactionFailure(message: e.message));
+      }
+      if (e is PaymentException) {
+        return Left(UserTransactionFailure(message: e.message));
+      }
       return const Left(ServerFailure());
     }
   }
--- test/features/transactions/repository/transactions_repository_test.dart
+++ test/features/transactions/repository/transactions_repository_test.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_app/core/error/failures.dart';
+import 'package:mobile_app/core/extensions/extensions.dart';
 import 'package:mobile_app/features/transactions/repository/transactions_repository.dart';
 import 'package:mobile_app/features/user_data/models/models.dart';
 import 'package:mocktail/mocktail.dart';
@@ -117,6 +118,123 @@ void main() {
         equals(const Left<Failure, List<UserTransaction>>(ServerFailure())),
       );
       verify(() => qvaPayApi.getTransactions()).called(1);
+    });
+  });
+
+  group('processTransaction', () {
+    final tUserTransactionCreated = tUserTransactionList[0];
+    final tUserTransactionResponse = tUserTransactionList[3];
+    final tTransactionQvaPayCreate = tTransactionsQvaPayList[1];
+    final tTransactionQvaPayPaid = tTransactionsQvaPayList[3];
+    test(
+        'should return [Right<UserTransaction>] when the transaction is '
+        'successfully created and paid', () async {
+      when(() => qvaPayApi.createTransaction(
+            uuid: tUserTransactionCreated.uuid,
+            amount: tUserTransactionCreated.amount.toDouble(),
+            description: tUserTransactionCreated.description,
+          )).thenAnswer((_) async => tTransactionQvaPayCreate);
+      when(() => qvaPayApi.payTransaction(
+            uuid: tTransactionQvaPayCreate.uuid,
+          )).thenAnswer((_) async => tTransactionQvaPayPaid);
+
+      final result = await repository.processTransaction(
+        transaction: tUserTransactionCreated,
+      );
+
+      expect(result, Right<Failure, UserTransaction>(tUserTransactionResponse));
+    });
+
+    test(
+        'should throw [Left<TransactionFailure>] when the account does '
+        'not have enough balance.', () async {
+      when(() => qvaPayApi.createTransaction(
+            uuid: tUserTransactionCreated.uuid,
+            amount: tUserTransactionCreated.amount.toDouble(),
+            description: tUserTransactionCreated.description,
+          )).thenAnswer((_) async => tTransactionQvaPayCreate);
+      when(() => qvaPayApi.payTransaction(
+            uuid: tTransactionQvaPayCreate.uuid,
+          )).thenThrow(
+        const TransactionException(message: 'Does not have enough balance.'),
+      );
+
+      final result = await repository.processTransaction(
+        transaction: tUserTransactionCreated,
+      );
+
+      expect(
+        result,
+        const Left<Failure, UserTransaction>(
+          UserTransactionFailure(message: 'Does not have enough balance.'),
+        ),
+      );
+    });
+
+    test('should throw [Left<TransactionFailure>] when the pin is wrong.',
+        () async {
+      when(() => qvaPayApi.createTransaction(
+            uuid: tUserTransactionCreated.uuid,
+            amount: tUserTransactionCreated.amount.toDouble(),
+            description: tUserTransactionCreated.description,
+          )).thenAnswer((_) async => tTransactionQvaPayCreate);
+      when(() => qvaPayApi.payTransaction(
+            uuid: tTransactionQvaPayCreate.uuid,
+          )).thenThrow(
+        const PaymentException(message: 'Incorrect PIN.'),
+      );
+
+      final result = await repository.processTransaction(
+        transaction: tUserTransactionCreated,
+      );
+
+      expect(
+        result,
+        const Left<Failure, UserTransaction>(
+          UserTransactionFailure(message: 'Incorrect PIN.'),
+        ),
+      );
+    });
+
+    test(
+        'should throw [Left<ServerFailure>] when an error occurs '
+        'on the server while creating the transaction', () async {
+      when(() => qvaPayApi.createTransaction(
+            uuid: tUserTransactionCreated.uuid,
+            amount: tUserTransactionCreated.amount.toDouble(),
+            description: tUserTransactionCreated.description,
+          )).thenThrow(ServerException());
+
+      final result = await repository.processTransaction(
+        transaction: tUserTransactionCreated,
+      );
+
+      expect(
+        result,
+        const Left<Failure, UserTransaction>(ServerFailure()),
+      );
+    });
+
+    test(
+        'should throw [Left<ServerFailure>] when an error occur on the '
+        'server in the payment', () async {
+      when(() => qvaPayApi.createTransaction(
+            uuid: tUserTransactionCreated.uuid,
+            amount: tUserTransactionCreated.amount.toDouble(),
+            description: tUserTransactionCreated.description,
+          )).thenAnswer((_) async => tTransactionQvaPayCreate);
+      when(() => qvaPayApi.payTransaction(
+            uuid: tTransactionQvaPayCreate.uuid,
+          )).thenThrow(ServerException());
+
+      final result = await repository.processTransaction(
+        transaction: tUserTransactionCreated,
+      );
+
+      expect(
+        result,
+        const Left<Failure, UserTransaction>(ServerFailure()),
+      );
     });
   });
 }
