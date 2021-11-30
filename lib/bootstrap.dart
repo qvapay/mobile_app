bca6f4b1540dc2ecfc04a302e707d15e74705ed5
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2021-11-30 12:51:33 -0600

    refactor: implement the `HydratedBlocOverrides` API in bootstrap file

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -8,21 +8,31 @@
 import 'dart:async';
 import 'dart:developer';
 
-import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mobile_app/app/app_bloc_observer.dart';
 import 'package:mobile_app/core/dependency_injection/dependency_injection.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
-  Bloc.observer = AppBlocObserver();
+  WidgetsFlutterBinding.ensureInitialized();
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
   await runZonedGuarded(
     () async {
-      await setUp();
-      runApp(await builder());
+      final storage = await HydratedStorage.build(
+        storageDirectory: await getTemporaryDirectory(),
+      );
+      await HydratedBlocOverrides.runZoned(
+        () async {
+          await setUp();
+          runApp(await builder());
+        },
+        storage: storage,
+        blocObserver: AppBlocObserver(),
+      );
     },
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
