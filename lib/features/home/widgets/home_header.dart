138f58d6258bac89e7083db8dad199e085ed6466
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2021-12-13 17:28:51 -0600

    feat(setting): add feature settings for theme handling

--- lib/features/home/widgets/home_header.dart
+++ lib/features/home/widgets/home_header.dart
@@ -1,13 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile_app/core/constants/widgets_constants.dart';
 import 'package:mobile_app/core/widgets/widgets.dart';
 import 'package:mobile_app/features/authentication/authentication.dart';
-import 'package:mobile_app/features/login/login.dart';
 import 'package:mobile_app/features/preferences/preferences.dart';
+import 'package:mobile_app/features/setting/setting.dart';
 import 'package:mobile_app/features/setting/theme/theme.dart';
-import 'package:mobile_app/features/start/start.dart';
 import 'package:mobile_app/features/user_data/user_data.dart';
 
 class HomeHeader extends StatelessWidget {
@@ -85,10 +83,18 @@ class HomeHeader extends StatelessWidget {
                                   const SizedBox(
                                     width: 10,
                                   ),
-                                  ProfileImageNetworkWidget(
-                                    imageUrl: state.userData!.logo,
-                                    borderImage: Border.all(
-                                        width: 4, color: Colors.white),
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.push<void>(
+                                        context,
+                                        SettingPage.go(),
+                                      );
+                                    },
+                                    child: ProfileImageNetworkWidget(
+                                      imageUrl: state.userData!.logo,
+                                      borderImage: Border.all(
+                                          width: 4, color: Colors.white),
+                                    ),
                                   ),
                                 ],
                               )
