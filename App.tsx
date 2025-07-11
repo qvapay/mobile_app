f673b0d7a3427df8fe05d19f95ad4dd1a3783318
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2025-07-11 16:43:51 -0400

    Refactor App component to implement navigation and update layout
    - Integrated React Navigation with a stack navigator for screen management.
    - Added SplashScreen and WelcomeScreen components as routes.
    - Replaced View with SafeAreaView for better layout handling.
    - Updated styles to include a background color.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,43 @@
-import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native'
+// React Components
+import { SafeAreaView, Platform, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native'
+
+// Navigation Components
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+const Stack = createNativeStackNavigator()
+
+// Routes
+import { ROUTES } from './components/screens/routes'
+
+// Screens
+import { SplashScreen } from './components/screens/SplashScreen'
+import { WelcomeScreen } from './components/screens/WelcomeScreen'
 
 function App() {
 
 	const isDarkMode = useColorScheme() === 'dark'
 
 	return (
-		<View style={styles.container}>
+		<SafeAreaView style={styles.container}>
 			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
 
-			<Text>Hello World</Text>
-		</View>
+			<NavigationContainer>
+				<Stack.Navigator
+					initialRouteName={ROUTES.SPLASH_SCREEN}
+				>
+					<Stack.Screen name={ROUTES.SPLASH_SCREEN} component={SplashScreen} />
+					<Stack.Screen name={ROUTES.WELCOME_SCREEN} component={WelcomeScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
+
+		</SafeAreaView>
 	)
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1
+		flex: 1,
+		backgroundColor: 'red'
 	}
 })
 
---
+++ components/screens/routes.js
@@ -0,0 +1,38 @@
+export const ROUTES = {
+
+    // Initial Screens
+    SPLASH_SCREEN: "Splash",
+    WELCOME_SCREEN: "Welcome",
+
+    // Stacks
+    AUTH_STACK: "AuthStack",
+    MAIN_STACK: "MainStack",
+    P2P_STACK: "P2PStack",
+    SETTINGS_STACK: "SettingsStack",
+    TRANSACTION_STACK: "TransactionStack",
+
+    // Shop Stack
+    SHOP_STACK: "ShopStack",
+
+    // Auth Screens
+    LOGIN_SCREEN: "Login",
+    REGISTER_SCREEN: "Register",
+
+    // Home Screens
+    HOME_SCREEN: "Home",
+    ADD_SCREEN: "Add",
+    ADD_INSTRUCTIONS_SCREEN: "AddInstructions",
+    WITHDRAW_SCREEN: "Withdraw",
+    WITHDRAW_INSTRUCTIONS_SCREEN: "WithdrawInstructions",
+    SCAN_SCREEN: "Scan",
+    SEND_SCREEN: "Send",
+    CONFIRM_SEND_SCREEN: "ConfirmSend",
+
+    // In out Screens
+    TRANSACTION_SCREEN: "Transaction",
+    P2P_SCREEN: "PeerToPeer",
+
+    // Profile & Settings Routes
+    SETTINGS_MENU: "SettingsMenu",
+    PROFILE_SCREEN: "Profile",
+}
