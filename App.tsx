d4c8c63f9d054175e2663eabbb28db726d6ce4d3
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2025-07-11 16:07:51 -0400

    Update project configuration and add navigation dependencies
    - Updated ESLint and Prettier configurations for consistency.
    - Modified App component to include a "Hello World" text.
    - Updated package version to 0.0.2 and added navigation dependencies: @react-navigation/native and @react-navigation/native-stack.
    - Cleaned up index.js and metro.config.js for better readability.
    - Removed README.md as it is no longer needed.

--- App.tsx
+++ App.tsx
@@ -1,28 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native'
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+	const isDarkMode = useColorScheme() === 'dark'
+
+	return (
+		<View style={styles.container}>
+			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
+			<Text>Hello World</Text>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+	container: {
+		flex: 1
+	}
+})
 
-export default App;
+export default App
