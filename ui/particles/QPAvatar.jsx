a8d7b3b6102c15d56659177bd47cddbe65e54cba
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2025-07-24 18:01:51 -0400

    Update QPAvatar component to improve image handling and styling
    - Added a new hasImage variable to determine if a user image is present.
    - Enhanced FastImage styling to set a background color when the user is VIP and no image is available, improving visual feedback.

--- ui/particles/QPAvatar.jsx
+++ ui/particles/QPAvatar.jsx
@@ -26,12 +26,18 @@ const QPAvatar = ({ user = {}, size = 32 }) => {
     const borderVip = size / 25
     const gradientColors = vip ? [theme.colors.danger, theme.colors.primary, theme.colors.success] : ['#ffffff', '#ffffff']
     const profile_picture = image ? `https://media.qvapay.com/${image}` : 'https://qvapay.com/android-chrome-512x512.png'
+    const hasImage = !!image
 
     return (
         <View style={[styles.container, { backgroundColor: theme.colors.background, width: size, height: size }]}>
             <LinearGradient colors={gradientColors} style={{ padding: borderVip, borderRadius: size }}>
                 <FastImage
-                    style={{ width: size - (borderVip * 2), height: size - (borderVip * 2), borderRadius: (size - (borderVip * 2)) / 2 }}
+                    style={{ 
+                        width: size - (borderVip * 2), 
+                        height: size - (borderVip * 2), 
+                        borderRadius: (size - (borderVip * 2)) / 2,
+                        backgroundColor: vip && !hasImage ? '#ffffff' : 'transparent'
+                    }}
                     source={{
                         uri: profile_picture,
                         priority: FastImage.priority.normal,
